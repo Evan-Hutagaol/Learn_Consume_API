aab7701056ae5371d6f1de6e43e5e53a33942f6f
evanhutagaol <evanhutagaol15@gmail.com>
2023-08-01 17:24:50 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Select from "react-select";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [datas, setDatas] = useState([]);
+  const [userSelect, setUserSelect] = useState ("")
+  const [isShow, setIsShow] = useState(false) //initialitation of variable for showing value
+  
+
+  const getBerries = async () => {
+    const berries = await fetch("https://pokeapi.co/api/v2/berry/");
+    const value = await berries.json();
+    const result = value.results.map((data) => {
+      return {
+        label: data.name,
+        value: data.name,
+      };
+    });
+    setDatas(result.sort((a, b) => a.label.localeCompare(b.label)))
+  };
+
+  useEffect(() => {
+    getBerries();
+  }, []);
+
+  // function for button if clicked it will show the value that choose by user
+  const handleSubmit = () =>{
+    setIsShow(state => !state)
+  }
+
+  const handleChange = (value) => {
+    setUserSelect(value)
+  }
+
+  return <div className="App">
+    {/* The value will store to the tag h1 */}
+    <h1>{isShow ? userSelect: ""}</h1> 
+    {/* Handle button to hide and show values */}
+    <button onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "Hide Value" : "Show Values"}</button>
+    <br/>
+    <br/>
+    {<Select options={datas} onChange={(e) => handleChange(e.value)}></Select>}</div>;
 }
 
 export default App;
